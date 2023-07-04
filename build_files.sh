783980d25062fdfdd1372534aea0d62e84702d75
Rehan Ahmed <73068765+Rehan-stack@users.noreply.github.com>
2023-07-04 20:19:28 +0500

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 echo "BUILD STATIC"
 python3.9 -m pip install -r requirements.txt
+pip install https://github.com/darklow/django-suit/tarball/v2
 python3.9 manage.py collectstatic 
 echo  "BUILD END"
