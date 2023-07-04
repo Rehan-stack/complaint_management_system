f3443c2ce23cc884238bdb18de1fa4e781aebf54
Rehan Ahmed <73068765+Rehan-stack@users.noreply.github.com>
2023-07-04 19:57:47 +0500

    Update settings.py

--- web/settings.py
+++ web/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/login_redirect/'
 LOGIN_URL = 'signin'
